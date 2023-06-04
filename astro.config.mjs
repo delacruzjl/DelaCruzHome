0c769c3a0ba97666ef4144b8683765c59d0778d8
Jose De la Cruz <jose@delacruzhome.com>
2023-06-03 21:55:26 -0400

    sitemap and build twice a day

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from 'astro/config';
 import storyblok from "@storyblok/astro";
 import { loadEnv } from 'vite';
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://delacruzhome.com',
   compressHTML: true,
   integrations: [react(), storyblok({
     accessToken: env.STORYBLOK_TOKEN,
     apiOptions: {
       region: 'us'
     }
-  })]
+  }), sitemap()]
 });
