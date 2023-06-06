fb3d392cf25b581854d37444f966c527946905d1
Jose DelaCruz <jodelac@microsoft.com>
2023-06-06 12:20:54 -0400

    swagger to az function

--- api/NewsletterSubscriber.cs
+++ api/NewsletterSubscriber.cs
@@ -4,11 +4,14 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System.Text.Json;
 using Api.Models;
 using SendGrid;
 using FluentValidation;
 using Api.Extensions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 
 namespace Api;
 
@@ -32,6 +35,10 @@ public class NewsletterSubscriber
     }
 
     [FunctionName("NewsletterSubscriber")]
+    [OpenApiOperation(operationId: "Subscribe", tags: new[] { "Subscribe" })]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]        
+    [OpenApiRequestBody(contentType: "application/json; charset=utf-8", bodyType: typeof(NewsletterContact), Description = "Sample Newsletter Contact", Required = true)]
+    [OpenApiResponseWithoutBody(statusCode: System.Net.HttpStatusCode.Accepted, Description = "The response submitted to SendGrid")]
     public async Task<IActionResult> Subscribe(
         [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req, ILogger _logger)
     {
--- api/Startup.cs
+++ api/Startup.cs
@@ -4,8 +4,12 @@ using Api.Models;
 using Api.Validators;
 using FluentValidation;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using SendGrid;
 
 [assembly: FunctionsStartup(typeof(Api.Startup))]
@@ -30,5 +34,37 @@ public class Startup : FunctionsStartup
         
         var apiKey = SendGridConfiguration.ApiKey;
         services.AddSingleton<ISendGridClient>(new SendGridClient(apiKey));
+
+        services.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+                            {
+                                var options = new OpenApiConfigurationOptions()
+                                {
+                                    Info = new OpenApiInfo()
+                                    {
+                                        Version = "1.0.0",
+                                        Title = "Swagger Jose's SendGrid Newsletter Subscription API",
+                                        Description = "API designed by [http://delacruzhome.com](http://delacruzhome.com).",
+                                        TermsOfService = new Uri("https://github.com/delacruzjl"),
+                                        Contact = new OpenApiContact()
+                                        {
+                                            Name = "Jose",
+                                            Email = Environment.GetEnvironmentVariable("SupportEmail"),
+                                            Url = new Uri("https://github.com/delacruzjl"),
+                                        },
+                                        License = new OpenApiLicense()
+                                        {
+                                            Name = "MIT",
+                                            Url = new Uri("http://opensource.org/licenses/MIT"),
+                                        }
+                                    },
+                                    Servers = DefaultOpenApiConfigurationOptions.GetHostNames(),
+                                    OpenApiVersion = OpenApiVersionType.V2,
+                                    IncludeRequestingHostName = true,
+                                    ForceHttps = false,
+                                    ForceHttp = false,
+                                };
+
+                                return options;
+                            });
     }
 }
