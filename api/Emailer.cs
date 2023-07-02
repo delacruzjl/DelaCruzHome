c676699c78feb03fb0adfb6c34845f7b46afaea6
Jose De la Cruz <jose@delacruzhome.com>
2023-07-02 13:52:05 -0400

    parametrize some constants

--- api/Emailer.cs
+++ api/Emailer.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Jodelac.SendGridAzFunction.Interfaces;
 using Jodelac.SendGridAzFunction.Models;
@@ -22,8 +23,8 @@ public class Emailer {
 
     [FunctionName("Emailer")]
     [OpenApiOperation(operationId: "Send", tags: new[] { "EmailerSend" })]
-    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]        
-    [OpenApiRequestBody(contentType: "application/json; charset=utf-8", bodyType: typeof(ContactForm), Description = "Sample SendGrid Message", Required = true)]
+    [OpenApiSecurity(ApiSwaggerInfo.TestFunctionApiKey, SecuritySchemeType.ApiKey, In = OpenApiSecurityLocationType.Header, Name = ApiSwaggerInfo.TestFunctionApiHeaderName)]        
+    [OpenApiRequestBody(contentType: $"{MediaTypeNames.Application.Json}; charset=utf-8", bodyType: typeof(ContactForm), Description = "Sample SendGrid Message", Required = true)]
     [OpenApiResponseWithoutBody(statusCode: System.Net.HttpStatusCode.NoContent, Description = "The email was submited through SendGrid API")]
     public async Task Send(
         [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req, 
